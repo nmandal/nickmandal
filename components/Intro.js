7563fb9310be0ffdfb0836507e6929d75d145d44
nmandal <nicholas.mandal@gmail.com>
2021-11-13 12:12:34 -0600

    test

--- components/Intro.js
+++ components/Intro.js
@@ -3,13 +3,9 @@ const Intro = () => {
       <div className="mt-6 prose prose-md text-gray-500 mx-auto">
         <h4>Nick Mandal</h4>
         <p>
-          I'm a software engineer at <a className="underline" target="_blank" href="https://www.bamfunds.com">Balyasny Asset Management</a>, a multi-strategy investment firm, where I work on the portfolio finance team.
+          I'm a software engineer at <a className="underline" target="_blank"
+            rel="noopener noreferrer" href="https://www.bamfunds.com">Balyasny Asset Management</a>, a multi-strategy investment firm, where I work on the portfolio finance team.
         </p>
-
-    {/* <div className="py-16 pb-5">
-      <h6 className="text-md leading-6 font-medium text-gray-400">Writing</h6>
-    </div> */}
-
       </div>
     )
   }
