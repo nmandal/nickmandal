d2a17a24bc81b41c8c242db30e7cb4f9aa8735d5
nmandal <nicholas.mandal@gmail.com>
2021-11-14 11:53:33 -0600

    pelonft

--- components/Intro.js
+++ components/Intro.js
@@ -1,14 +1,18 @@
-const Intro = () => {
-    return (
-      <div className="mt-6 prose prose-md text-gray-500 mx-auto">
-        <h4>Nick Mandal</h4>
-        <p>
-          I&apos;m a software engineer at <a className="underline" target="_blank"
-            rel="noopener noreferrer" href="https://www.bamfunds.com">Balyasny Asset Management</a>, 
-            a multi-strategy investment firm, where I work on the portfolio finance team.
-        </p>
+export default function Intro () {
+  return (
+    <div className="mt-6 prose prose-md text-gray-500 mx-auto">
+      <h4>Nick Mandal</h4>
+      <p>
+        I&apos;m a software engineer at <a className="underline" target="_blank"
+        rel="noopener noreferrer" href="https://www.bamfunds.com">Balyasny Asset Management</a>, 
+        a multi-strategy investment firm, where I work on the portfolio finance team.
+      </p>
+      <p>
+        In my free time, I do contract work for startups, build web3 applications and track my health.
+      </p>
+      <div className="mt-5">
+        <a href="/pelonft">PeloNFT</a>
       </div>
-    )
-  }
-
-export default Intro;
+    </div>
+  )
+}
