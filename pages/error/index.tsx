b987e6739de8b96192174175396afe6d54be8b92
nmandal <nicholas.mandal@gmail.com>
2021-11-20 15:26:24 -0600

    fix port

--- pages/error/index.tsx
+++ pages/error/index.tsx
@@ -8,8 +8,7 @@ const ErrorPage = () => {
     <div className="prose mx-auto mt-16">
       <h1> Error:{router.query.error}! </h1>
       <p>
-        <br /> Make sure that you purchased the course with this email. If you
-        did and you can&apos;t login, please contact support :D
+        <br /> Something is off...
       </p>
       <br />
     </div>
