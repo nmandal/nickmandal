793385da308fe01b88cde3e1e3c372c091d47643
nmandal <nicholas.mandal@gmail.com>
2021-11-20 15:43:33 -0600

    env var was problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,18 @@ export default function Home() {
   const {data: session} = useSession();
   return (
     <div className="relative px-4 sm:px-6 lg:px-8">
+            <>
+      {session ?
+        <>
+        <p>Signed in as {session.user.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+        </>
+        :
+        <>
+{/* <button onClick={() => signIn()}>Sign in</button> */}
+        </>
+      }
+      </>
       <Intro />
     </div>
   )
