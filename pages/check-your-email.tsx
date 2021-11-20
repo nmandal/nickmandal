993f9507c6bdc4580bb5589ed40446e6c86acbd0
nmandal <nicholas.mandal@gmail.com>
2021-11-20 13:15:18 -0600

    test

--- pages/check-your-email.tsx
+++ pages/check-your-email.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CheckYourEmail = () => {
   return (
     <div className="prose mx-auto my-16">
-      <h1>Check your email!</h1>
+      <h1>Check your  email!</h1>
     </div>
   );
 };
